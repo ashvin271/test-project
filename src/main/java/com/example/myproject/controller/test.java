eccebb5cd41c7bf08b72dfcd48c4850fdd8eed34
ashvin.patidar@divergentsl.com <ashvin.patidar@divergentsl.com>
2022-05-16 11:14:21 -0500

    both property fiels

--- src/main/java/com/example/myproject/controller/test.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.controller;
-
-public class test {
-  public static void main(String[] args) {
-	System.out.println("hello word");
-	System.out.println("test");
-	System.out.println("test");
-}
-}
